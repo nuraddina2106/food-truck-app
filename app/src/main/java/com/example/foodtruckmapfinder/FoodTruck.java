b3ea4056e49255b36eb49b4c122c0d829385222f
NURADDINA IZZAH HAMIZI <nuraddinaizzah21062002@gmail.com>
2024-07-20 18:39:20 +0800

    Update FoodTruck.java

--- app/src/main/java/com/example/foodtruckmapfinder/FoodTruck.java
+++ app/src/main/java/com/example/foodtruckmapfinder/FoodTruck.java
@@ -1,52 +1,36 @@
 package com.example.foodtruckmapfinder;
-
-import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 public class FoodTruck {
 
     @SerializedName("truck_id")
-    @Expose
     private String truckId;
-    @SerializedName("business_type")
-    @Expose
-    private String businessType;
-    @SerializedName("name")
-    @Expose
-    private String name;
-    @SerializedName("operator_name")
-    @Expose
-    private String operatorName;
-    @SerializedName("address")
-    @Expose
-    private String address;
-    @SerializedName("business_hours")
-    @Expose
-    private String businessHours;
-    @SerializedName("latitude")
-    @Expose
-    private String latitude;
-    @SerializedName("longitude")
-    @Expose
-    private String longitude;
-    @SerializedName("image")
-    @Expose
-    private String image;
 
+    @SerializedName("name")
+    private String name;
+
+    @SerializedName("operator_name")
+    private String operatorName;
+
+    @SerializedName("menu_name")
+    private String[] menuNames;
+
+    @SerializedName("business_hours")
+    private String businessHours;
+
+    @SerializedName("latitude")
+    private String latitude;
+
+    @SerializedName("longitude")
+    private String longitude;
+
+    // Getters and setters
     public String getTruckId() {
         return truckId;
     }
 
     public void setTruckId(String truckId) {
         this.truckId = truckId;
-    }
-
-    public String getBusinessType() {
-        return businessType;
-    }
-
-    public void setBusinessType(String businessType) {
-        this.businessType = businessType;
     }
 
     public String getName() {
@@ -65,12 +49,12 @@ public class FoodTruck {
         this.operatorName = operatorName;
     }
 
-    public String getAddress() {
-        return address;
+    public String[] getMenuNames() {
+        return menuNames;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
+    public void setMenuNames(String[] menuNames) {
+        this.menuNames = menuNames;
     }
 
     public String getBusinessHours() {
@@ -96,13 +80,4 @@ public class FoodTruck {
     public void setLongitude(String longitude) {
         this.longitude = longitude;
     }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
 }
